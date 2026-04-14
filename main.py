dab9c083f891861be76731e0ba05e67d1baf3b7b
NOooONGiB <noon.ahead@gmail.com>
2026-04-14 14:10:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 # print heelo world
-print("Hello World")  
-
-# ptiny good bye
-print("Good Bye")
-
-#new function
-def add_two(num):
-    return num + 2
-
-print(add_two(2), add_two(5))
+print("Now I know what git and github is. EASY!!!")  
